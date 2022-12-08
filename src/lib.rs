bf58c2d2aa4736c47bc36cafe225ee1ee4fe5ef6
Rod Conan <rconan@gmto.org>
2022-12-08 12:35:42 -0800

    INSIDE PUPIL
    Add inside_pupil method to Observer trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
+use std::path::Path;
+
+use image::{ImageResult, Rgb, RgbImage};
 use num_complex::Complex;
+use num_traits::Zero;
 
 mod zpdft;
 pub use zpdft::ZpDft;
@@ -14,6 +18,53 @@ pub use objects::{Objects, Star, StarDistribution};
 pub trait Observer {
     fn diameter(&self) -> f64;
     fn resolution(&self) -> f64;
-    fn pupil(&self, shift: Option<(f64, f64)>) -> Vec<Complex<f64>>;
-    fn show_pupil<P: AsRef<std::path::Path>>(&self, path: Option<P>) -> image::ImageResult<()>;
+    fn inside_pupil(&self, x: f64, y: f64) -> bool;
+
+    fn pupil(&self, shift: Option<(f64, f64)>) -> Vec<Complex<f64>> {
+        let diameter = self.diameter();
+        let n_px = (diameter / self.resolution()).round() as usize + 1;
+        let l = (n_px - 1) as f64;
+        let mut buffer: Vec<Complex<f64>> = vec![Complex::zero(); n_px * n_px];
+        if let Some((hx, hy)) = shift {
+            for i in 0..n_px {
+                let x = (i as f64 / l - 0.5) * diameter;
+                for j in 0..n_px {
+                    let y = (j as f64 / l - 0.5) * diameter;
+                    if self.inside_pupil(x, y) {
+                        let k = i * n_px + j;
+                        buffer[k] = Complex::new(1f64, 0f64)
+                            * Complex::from_polar(
+                                1f64,
+                                -2. * std::f64::consts::PI * (x * hx + y * hy),
+                            );
+                    }
+                }
+            }
+        } else {
+            for i in 0..n_px {
+                let x = (i as f64 / l - 0.5) * diameter;
+                for j in 0..n_px {
+                    let y = (j as f64 / l - 0.5) * diameter;
+                    if self.inside_pupil(x, y) {
+                        let k = i * n_px + j;
+                        buffer[k] = Complex::new(1f64, 0f64);
+                    }
+                }
+            }
+        }
+        buffer
+    }
+    fn show_pupil<P: AsRef<Path>>(&self, path: Option<P>) -> ImageResult<()> {
+        let n = (self.diameter() / self.resolution()).round() as u32 + 1;
+        let mut img = RgbImage::new(n, n);
+        img.pixels_mut()
+            .zip(self.pupil(None).into_iter())
+            .filter(|(_, pup)| pup.norm() > 0f64)
+            .for_each(|(px, _)| *px = Rgb([255, 255, 255]));
+        img.save(
+            path.as_ref()
+                .map(|p| p.as_ref())
+                .unwrap_or(Path::new("telescope_pupil.png")),
+        )
+    }
 }
--- src/telescope/generic.rs
+++ src/telescope/generic.rs
@@ -1,9 +1,4 @@
-use std::path::Path;
-
 use crate::Observer;
-use image::{ImageResult, Rgb, RgbImage};
-use num_complex::Complex;
-use rustfft::num_traits::Zero;
 
 /// Generic circular telescope
 #[derive(Debug)]
@@ -62,55 +57,10 @@ impl Observer for Telescope {
         self.resolution
     }
 
-    fn pupil(&self, shift: Option<(f64, f64)>) -> Vec<Complex<f64>> {
-        let n_px = (self.diameter / self.resolution).round() as usize + 1;
-        let l = (n_px - 1) as f64;
+    fn inside_pupil(&self, x: f64, y: f64) -> bool {
         let r_outer = self.diameter * 0.5;
         let r_inner = self.obscuration.unwrap_or_default();
-        let mut buffer: Vec<Complex<f64>> = vec![Complex::zero(); n_px * n_px];
-        if let Some((hx, hy)) = shift {
-            for i in 0..n_px {
-                let x = (i as f64 / l - 0.5) * self.diameter;
-                for j in 0..n_px {
-                    let y = (j as f64 / l - 0.5) * self.diameter;
-                    let r = x.hypot(y);
-                    if r >= r_inner && r <= r_outer {
-                        let k = i * n_px + j;
-                        buffer[k] = Complex::new(1f64, 0f64)
-                            * Complex::from_polar(
-                                1f64,
-                                -2. * std::f64::consts::PI * (x * hx + y * hy),
-                            );
-                    }
-                }
-            }
-        } else {
-            for i in 0..n_px {
-                let x = (i as f64 / l - 0.5) * self.diameter;
-                for j in 0..n_px {
-                    let y = (j as f64 / l - 0.5) * self.diameter;
-                    let r = x.hypot(y);
-                    if r >= r_inner && r <= r_outer {
-                        let k = i * n_px + j;
-                        buffer[k] = Complex::new(1f64, 0f64);
-                    }
-                }
-            }
-        }
-        buffer
-    }
-
-    fn show_pupil<P: AsRef<Path>>(&self, path: Option<P>) -> ImageResult<()> {
-        let n = (self.diameter / self.resolution).round() as u32 + 1;
-        let mut img = RgbImage::new(n, n);
-        img.pixels_mut()
-            .zip(self.pupil(None).into_iter())
-            .filter(|(_, pup)| pup.norm() > 0f64)
-            .for_each(|(px, _)| *px = Rgb([255, 255, 255]));
-        img.save(
-            path.as_ref()
-                .map(|p| p.as_ref())
-                .unwrap_or(Path::new("telescope_pupil.png")),
-        )
+        let r = x.hypot(y);
+        r >= r_inner && r <= r_outer
     }
 }
